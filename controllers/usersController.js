763b53f4aff4e6d7e6065464832582958cdbe2e8
EricM76 <53233372+EricM76@users.noreply.github.com>
2021-08-06 15:08:44 -0300

    acomoda formulario de registro

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,10 +1,15 @@
-const productos = require('../data/products_db');
+const {productos} = require('../data/products_db');
+const paises = require('../data/paises');
 
 module.exports = {
     register : (req,res) => {
         return res.render('register',{
-            productos
+            productos,
+            paises
         })
+    },
+    processRegister : (req,res) => {
+        return res.send(req.body)
     },
     login : (req,res) => {
         return res.render('login',{
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-const {register,login} = require('../controllers/usersController');
+const {register,login, processRegister} = require('../controllers/usersController');
 
 /* GET users listing. */
 router.get('/register', register);
+router.post('/register', processRegister)
 router.get('/login', login);
 
 module.exports = router;
